ca8b2d8ab7c8fbe7b55f9c6ca5001af10b323191
tnvinh00 <tnvinh99@gmail.com>
2021-06-30 21:11:53 +0700

    Vinh feature receive notification from firebase clouding

--- App.js
+++ App.js
@@ -6,10 +6,37 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Providers from './navigation';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
+    const getToken = async () => {
+        try {
+            const token = await messaging().getToken();
+        console.log(token);
+        } catch (error) {
+            console.log(error);
+        }
+    } 
+
+    useEffect(() => {
+        getToken();
+        messaging().onMessage(async remoteMessage => {
+            console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+        });
+        messaging().onNotificationOpenedApp(remoteMessage => {
+            console.log('Notification caused app to open from background state:', JSON.stringify(remoteMessage));
+        });
+        messaging()
+            .getInitialNotification()
+            .then(remoteMessage => {
+                if (remoteMessage) {
+                    console.log('Notification caused app to open from quit state:', JSON.stringify(remoteMessage));
+                }
+            })
+            .catch((e) => console.log(e));
+    }, []);
     return <Providers />;
 };
 
