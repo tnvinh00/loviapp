4f34ab85a14f7a504c02a811915d9ad19fbb1c21
tnvinh00 <tnvinh99@gmail.com>
2021-06-30 21:06:05 +0700

    Vinh like and follow post on postcard

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import LottieView from 'lottie-react-native';
 
 import {
     Container,
@@ -22,8 +23,9 @@ import ProgressiveImage from './ProgressiveImage';
 import { AuthContext } from '../navigation/AuthProvider';
 
 import moment from 'moment';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import firestore from '@react-native-firebase/firestore';
+import { windowWidth } from '../utils/Dimentions.js'
 
 moment.updateLocale('en', {
     relativeTime: {
@@ -46,27 +48,98 @@ moment.updateLocale('en', {
     }
 });
 
-const PostCard = ({ onSeePhoto, item, onDelete, onPress }) => {
+const PostCard = ({ onSeePhoto, item, username, onDelete, onPress, onComment, isComment }) => {
     const { user, logout } = useContext(AuthContext);
+    const [liked, setLiked] = useState(false);
+    const [countlike, setCountlike] = useState(null);
+    const [countcomment, setCountcomment] = useState(null);
     const [userData, setUserData] = useState(null);
+    const [view, setView] = useState(false);
 
-    likeIcon = item.liked ? 'heart' : 'heart-outline';
-    likeIconColor = item.liked ? '#2e64e5' : '#333';
-
-    if (item.likes == 1) {
-        likeText = '1 Like';
-    } else if (item.likes > 1) {
-        likeText = item.likes + ' Likes';
-    } else {
-        likeText = 'Like';
+    const handleLike = async () => {
+        if (!liked) {
+            setView(true);
+            await firestore()
+                .collection("POSTS")
+                .doc(item.id)
+                .collection("LIKES")
+                .doc(user.uid)
+                .set({
+                    like: true
+                })
+                if (user.uid != item.userId) {
+                    await firestore()
+                        .collection('USERS')
+                        .doc(item.userId)
+                        .collection('NOTIFICATIONS')
+                        .add({
+                            notification: username + ' đã yêu thích bài viết của bạn',
+                            createdAt: firestore.Timestamp.fromDate(new Date()),
+                            type: 'like',
+                            postId: item.id,
+                            postUserId: item.userId,
+                        })
+                    var getnoti = 0;
+                    await firestore()
+                        .collection('USERS')
+                        .doc(item.userId)
+                        .get()
+                        .then((documentSnapshot) => {
+                            if (documentSnapshot.exists) {
+                                getnoti = documentSnapshot.data().notify;
+                            }
+                        })
+                    await firestore()
+                        .collection('USERS')
+                        .doc(item.userId)
+                        .update({
+                            notify: getnoti + 1
+                        })
+                }
+        }
+        else {
+            await firestore()
+                .collection("POSTS")
+                .doc(item.id)
+                .collection("LIKES")
+                .doc(user.uid)
+                .delete();
+        }
     }
 
-    if (item.comments == 1) {
-        commentText = '1 Comment';
-    } else if (item.comments > 1) {
-        commentText = item.comments + ' Comments';
-    } else {
-        commentText = 'Comment';
+    const checkLiked = async () => {
+        await firestore()
+            .collection("POSTS")
+            .doc(item.id)
+            .collection("LIKES")
+            .doc(user.uid)
+            .onSnapshot((documentSnapshot) => {
+                if (documentSnapshot.exists) {
+                    setLiked(true);
+                }
+                else
+                    setLiked(false);
+            })
+    }
+
+    const getNumOfLiked = async () => {
+        await firestore()
+            .collection("POSTS")
+            .doc(item.id)
+            .collection("LIKES")
+            .onSnapshot((querySnapshot) => {
+                setCountlike(querySnapshot.size);
+            })
+    }
+
+    const getNumOfCommented= async () => {
+        await firestore()
+            .collection("POSTS")
+            .doc(item.id)
+            .collection("COMMENTS")
+            .onSnapshot((querySnapshot) => {
+                setCountcomment(querySnapshot.size);
+            })
     }
 
     const getUser = async () => {
@@ -87,10 +160,13 @@ const PostCard = ({ onSeePhoto, item, onDelete, onPress }) => {
 
     useEffect(() => {
         getUser();
+        getNumOfLiked();
+        getNumOfCommented();
+        checkLiked();
     }, []);
 
     return (
-        <Card style={{width: '100%'}} key={item.id}>
+        <Card style={{ width: windowWidth-16 }} key={item.id} onPress={onComment}>
             <UserInfo>
                 <TouchableOpacity onPress={onPress}>
                     <UserImg
@@ -109,37 +185,57 @@ const PostCard = ({ onSeePhoto, item, onDelete, onPress }) => {
                     <PostTime>{moment(item.postTime.toDate()).fromNow()}</PostTime>
                 </UserInfoText>
             </UserInfo>
-            <PostText>{item.post}</PostText>
+            {item.post ? <PostText>{item.post}</PostText> : null}
             {/* {item.postImg != null ? <PostImg source={item.postImg} /> : <Divider />} */}
             {item.postImg != null ? (
-                <TouchableOpacity onPress={onSeePhoto}>
+                <TouchableWithoutFeedback onPress={onSeePhoto} style={{}}>
                     <ProgressiveImage
                         defaultImageSource={require('../assets/default-img.jpg')}
                         source={{ uri: item.postImg }}
                         style={{ width: '100%', height: 250 }}
                         resizeMode="cover"
                     />
-                </TouchableOpacity>
-
+                    {view ?
+                        <LottieView
+                            source={require('../assets/splash/836-like-button.json')}
+                            autoPlay
+                            style={{ width: 100, position: 'absolute', alignSelf: 'center', marginTop: 35 }}
+                            loop={false}
+                            speed={0.8}
+                            onAnimationFinish={() => {
+                                setView(false);
+                            }}
+                        />
+                        : null}
+                </TouchableWithoutFeedback >
             ) : (
                 <Divider />
             )}
-
-            <InteractionWrapper>
-                <Interaction active={item.liked}>
-                    <Ionicons name={likeIcon} size={25} color={likeIconColor} />
-                    <InteractionText active={item.liked}>{likeText}</InteractionText>
-                </Interaction>
-                <Interaction>
-                    <Ionicons name="md-chatbubble-outline" size={25} />
-                    <InteractionText>{commentText}</InteractionText>
-                </Interaction>
-                {user.uid == item.userId ? (
-                    <Interaction onPress={() => onDelete(item.id)}>
-                        <Ionicons name="md-trash-bin" size={25} />
+            {!isComment ? (
+                <InteractionWrapper>
+                    <Interaction active={liked} onPress={handleLike}>
+                        {liked ? (
+                            <Ionicons name={'heart'} size={25} color={'#de4d41'} />
+                        ) : (
+                            <Ionicons name={'heart-outline'} size={25} color={'#333'} />
+                        )}
+                        <InteractionText active={liked}>{countlike > 0 ? countlike : null} Yêu thích</InteractionText>
                     </Interaction>
-                ) : null}
-            </InteractionWrapper>
+                    <Interaction onPress={onComment}>
+                        {countcomment > 0 ? (
+                            <Ionicons name="chatbox-ellipses-outline" size={25} />
+                        ) : (
+                            <Ionicons name="chatbox-outline" size={25} />
+                        )}
+                        <InteractionText>{countcomment > 0 ? countcomment : null} Bình luận</InteractionText>
+                    </Interaction>
+                    {user.uid == item.userId ? (
+                        <Interaction onPress={() => onDelete(item.id)}>
+                            <Ionicons name="trash-outline" size={25} />
+                        </Interaction>
+                    ) : null}
+                </InteractionWrapper>
+            ) : null}
         </Card>
     );
 };
