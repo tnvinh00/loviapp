495a44c109ef187b5b8310aef7dbee46ac4c38ca
HoangNam1910 <18521120@gm.uit.edu.vn>
2021-06-30 20:49:23 +0700

    commit_Nam edit header + show number of notice

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, StyleSheet, FlatList, Alert, ActivityIndicator, SafeAreaView, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Alert, ActivityIndicator, SafeAreaView, ScrollView, TouchableOpacity, Image } from 'react-native';
 import FormButton from '../components/FormButton';
 
 import { AuthContext } from '../navigation/AuthProvider';
@@ -34,6 +34,9 @@ const HomeScreen = ({ navigation }) => {
     const [posts, setPosts] = useState(null);
     const [loading, setLoading] = useState(true);
     const [deleted, setDeleted] = useState(false);
+    const [notify, setNotify] = useState(0);
+    const [onget, setOnget] = useState(true);
+    const [username, setUsername] = useState(null);
 
     const ref = React.useRef(null);
     useScrollToTop(ref);
@@ -125,7 +128,7 @@ const HomeScreen = ({ navigation }) => {
                         text1: 'Xóa thành công',
                         visibilityTime: 3000,
                         autoHide: true,
-                        topOffset: 25,
+                        topOffset: 35,
                         bottomOffset: 40,
                     });
                 }
@@ -146,8 +149,80 @@ const HomeScreen = ({ navigation }) => {
             })
     }
 
+    const getNotify = async () => {
+        await firestore()
+            .collection('USERS')
+            .doc(user.uid)
+            .onSnapshot((documentSnapshot) => {
+                if (documentSnapshot.exists){
+                    setNotify(documentSnapshot.data().notify);
+                    setHeader();
+                    setOnget(false);
+                }
+            })
+    }
+    const getCurrentUser = async () => {
+        const currentUser = await firestore()
+            .collection('USERS')
+            .doc(user.uid)
+            .get()
+            .then((documentSnapshot) => {
+                if (documentSnapshot.exists) {
+                    setUsername(documentSnapshot.data().name);
+                }
+            })
+            .catch((e) => {
+                console.log(e);
+            })
+    }
+
+    const setHeader = () => {
+        navigation.setOptions({
+            title: '',
+            headerTitleAlign: 'left',
+            headerTitle: () => (
+                <Image style={{ height: 52, width: 150, marginTop: 10, marginLeft: -10 }}
+                    source={require('../assets/lovi-logo.png')}
+                />
+            ),
+            headerTitleStyle: {
+                color: '#2e64e5',
+                fontFamily: 'Kufam-SemiBoldItalic',
+                fontSize: 18,
+            },
+            headerStyle: {
+                shadowColor: '#fff',
+                height: 45,
+                elevation: 0,
+            },
+            headerRight: () => (
+                <View style={{ flexDirection: 'row', marginRight: 15 }}>
+                    <TouchableOpacity style={{ marginTop: 3, marginRight: 10 }} onPress={() => navigation.navigate('SearchProfile')}>
+                        <Ionicons name="search-circle" size={45} color="#608aeb" />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ marginTop: 5, marginRight: 10 }} onPress={() => navigation.navigate('Notify')}>
+                        <Ionicons name="notifications-circle" size={43} color="#608aeb" />
+                        {notify > 0 ? (
+                            <View style={styles.badge}>
+                                <Text style={styles.textbadge}>{notify > 9 ? '9+' : notify}</Text>
+                            </View>
+                        ) : null}
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ marginTop: 5 }} onPress={() => navigation.navigate('AddPost')}>
+                        <Ionicons name="add-circle" size={42} color="#608aeb" />
+                    </TouchableOpacity>
+                </View>
+            ),
+        })
+    }
+
+    useEffect(() => {
+        getNotify();
+    }, [onget]);
+
     useEffect(() => {
         fetchPosts();
+        getCurrentUser();
     }, [loading]);
 
     useEffect(() => {
@@ -164,7 +239,7 @@ const HomeScreen = ({ navigation }) => {
             {loading ? (
                 <ScrollView
                     style={{ flex: 1 }}
-                    contentContainerStyle={{ alignItems: 'center' }}
+                    contentContainerStyle={{ alignItems: 'center', margin: 0, padding: 10 }}
                     showsVerticalScrollIndicator={false}>
                     <SkeletonPlaceholder>
                         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
@@ -199,10 +274,10 @@ const HomeScreen = ({ navigation }) => {
                         <View style={{ marginTop: 10, marginBottom: 30 }}>
                             <View style={{ width: 300, height: 20, borderRadius: 4 }} />
                             <View
-                                style={{ marginTop: 6, width: 250, height: 20, borderRadius: 4 }}
+                                style={{ marginTop: 6, width: 350, height: 200, borderRadius: 4 }}
                             />
                             <View
-                                style={{ marginTop: 6, width: 350, height: 200, borderRadius: 4 }}
+                                style={{ marginTop: 6, width: 150, height: 20, borderRadius: 4 }}
                             />
                         </View>
                     </SkeletonPlaceholder>
@@ -212,18 +287,22 @@ const HomeScreen = ({ navigation }) => {
                     <Toast style={{ zIndex: 5, top: -30 }} ref={(ref) => Toast.setRef(ref)} />
                     <FlatList
                         ref={ref}
-                        onRefresh={() => fetchPosts()}
+                        onRefresh={() => {fetchPosts(); getCurrentUser(); setHeader();}}
                         refreshing={false}
                         data={posts}
                         renderItem={({ item }) => (
                             <PostCard
                                 item={item}
                                 onDelete={handleDelete}
+                                username={username}
                                 onPress={() =>
                                     navigation.navigate('HomeProfile', { userId: item.userId })
                                 }
                                 onSeePhoto={() => {
                                     navigation.navigate('Photo', { postImg: item.postImg, isAva: false })
+                                }}
+                                onComment={() => {
+                                    navigation.navigate('Comment', { postId: item.id, postUserId: item.userId, isComment: true })
                                 }}
                             />
                         )}
@@ -252,5 +331,21 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 20,
         color: '#333333'
+    },
+    badge: {
+        position: 'absolute',
+        top: 0,
+        flex: 1,
+        justifyContent: 'center',
+        right: 0,
+        backgroundColor: 'red',
+        width: 20,
+        height: 20,
+        borderRadius: 10,
+    },
+    textbadge: {
+        fontSize: 12,
+        textAlign: 'center',
+        color: 'white',
     }
 });
